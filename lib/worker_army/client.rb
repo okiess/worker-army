17e2db7456d5e4f2342281ed369a38dc3e505cfa
Oliver Kiessler <kiessler@inceedo.com>
2013-10-27 15:07:45 +0100

    1.8 Hash Syntax

--- lib/worker_army/client.rb
+++ lib/worker_army/client.rb
@@ -23,11 +23,11 @@ module WorkerArmy
       worker_army_base_url = @config['endpoint']
       callback_url = "#{worker_army_base_url}/generic_callback" unless callback_url
       response = RestClient.post "#{worker_army_base_url}/jobs",
-        data.merge(
-          job_class: job_class,
-          callback_url: "#{worker_army_base_url}/callback?callback_url=#{callback_url}",
-          queue_name: queue_name
-        ).to_json,
+        data.merge({
+          :job_class => job_class,
+          :callback_url => "#{worker_army_base_url}/callback?callback_url=#{callback_url}",
+          :queue_name => queue_name
+        }).to_json,
         :content_type => :json, :accept => :json
       response.code == 200
     end
