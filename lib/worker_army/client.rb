78d377933050b7ef5fb41ce61a3a7a99a7e24274
Oliver Kiessler <kiessler@inceedo.com>
2014-05-26 18:03:46 +0200

    Client retries

--- lib/worker_army/client.rb
+++ lib/worker_army/client.rb
@@ -4,16 +4,16 @@ require "multi_json"
 
 module WorkerArmy
   class Client
-    def self.push_job(job_class, data = {}, callback_url = nil, queue_name = 'queue')
+    def self.push_job(job_class, data = {}, callback_url = nil, queue_name = 'queue', retry_count = 0)
       raise "No data" unless data
       raise "No job class provided" unless job_class
       
       if ENV['worker_army_endpoint']
-        puts "Using environment variables for config..."
+        # puts "Using environment variables for config..."
         @config = { endpoint: ENV['worker_army_endpoint'] }
       else
         begin
-          puts "Using config in your home directory"
+          # puts "Using config in your home directory"
           @config = YAML.load(File.read("#{ENV['HOME']}/.worker_army.yml"))
         rescue Errno::ENOENT
           raise "worker_army.yml expected in ~/.worker_army.yml"
@@ -22,14 +22,24 @@ module WorkerArmy
 
       worker_army_base_url = @config['endpoint']
       callback_url = "#{worker_army_base_url}/generic_callback" unless callback_url
-      response = RestClient.post "#{worker_army_base_url}/jobs",
-        data.merge(
-          job_class: job_class,
-          callback_url: "#{worker_army_base_url}/callback?callback_url=#{callback_url}",
-          queue_name: queue_name
-        ).to_json,
-        :content_type => :json, :accept => :json
-      response.code == 200
+      response = nil
+      begin
+        response = RestClient.post "#{worker_army_base_url}/jobs",
+          data.merge(
+            job_class: job_class,
+            callback_url: "#{worker_army_base_url}/callback?callback_url=#{callback_url}",
+            queue_name: queue_name
+          ).to_json,
+          :content_type => :json, :accept => :json
+      rescue => e
+        puts "Failed! Retrying (#{retry_count})..."
+        retry_count += 1
+        if retry_count < (@config['client_retry_count'] ? @config['client_retry_count'].to_i : 10)
+          sleep (retry_count * 2)
+          push_job(job_class, data, callback_url, queue_name, retry_count)
+        end
+      end
+      (response and response.code == 200)
     end
   end
 end
