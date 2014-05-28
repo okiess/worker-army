4cac2ffb8760fd78300fea2d1e957af58880e202
Oliver Kiessler <kiessler@inceedo.com>
2014-05-28 17:50:53 +0200

    Logging

---
+++ lib/worker-army.rb
@@ -0,0 +1,8 @@
+require "redis"
+require "logger"
+
+require File.dirname(__FILE__) + '/worker_army/log'
+require File.dirname(__FILE__) + '/worker_army/queue'
+require File.dirname(__FILE__) + '/worker_army/worker'
+require File.dirname(__FILE__) + '/worker_army/client'
+require File.dirname(__FILE__) + '/worker_army/example_job'
